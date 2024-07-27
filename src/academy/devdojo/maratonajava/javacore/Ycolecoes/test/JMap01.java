3cdc4fe686d788379f641a1d3167d7e4f8832c0f
Ryan Souza Rodrigues <154678188+ryan-rodrigues@users.noreply.github.com>
2024-07-27 09:40:57 -0300

    .

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/JMap01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/JMap01.java
@@ -1,41 +1,46 @@
 package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class JMap01 {
     public static void main(String[] args) {
 
-        //Para cada "key" eu tenho um "value": Map <K,V>;
-
-        Map<String, String> map = new HashMap<>();//HashMap, LinkedHashMap(mantém ordem de inserção)
+        Map<String, String> map = new HashMap<>();//@ HashMap, LinkedHashMap(mantém ordem de inserção)
         map.put("teklado", "teclado");
-        map.put("mousse", "mouse");//<- posso ter elementos como o mesmo valor.
-        map.put("mouze", "mouse");//<- posso ter elementos como o mesmo valor.
+        map.put("mousse", "mouse");     //* posso ter elementos como o mesmo valor.
+        map.put("mouze", "mouse");      //* posso ter elementos como o mesmo valor.
         map.put("vc", "você2");
-        map.put("vc", "você");//sobrescreve o valor da chave já existente
-        map.putIfAbsent("vc", "você3");//adiciona caso a chave não tenha valor
+        map.put("vc", "você");          //* sobrescreve o valor da chave já existente
+        map.putIfAbsent("vc", "você3"); //* adiciona caso a chave não tenha valor
 
 
+        map.keySet();       //? retorna uma coleção Set (ou seja, sem elementos duplicados), com todas as chaves.
+        map.values();       //? retorna uma coleção Collection com todos os valores.
+        map.get("vc");      //? retorna o valor da chave passado como argumento.
+        map.entrySet();     //? retorna um Set com um Entry contendo todas as chaves e valores.
 
+//-----------------------------------------------------------------------------------------------------------------------------
 
-        System.out.println("Chaves: " + map.keySet());
-        System.out.println("Valores: " + map.values());
+        //! COMO FUNCIONA O ENTRY-SEwT:
 
-        System.out.println("------------------------------------------------\n" +
-                           "Pegando valor via chave com for-each: ");
-        for (String key : map.keySet()) {
-            System.out.println(key + ": " + map.get(key));
+        Set<Map.Entry<String, String>> entries = map.entrySet(); /// retorna um Set, com um Map.Entry contendo as chaves e valores.
+
+        for (Map.Entry<String, String> entry : entries) {   /// posso iterar pelo Set, usando uma variável Map.Entry
+            //* O tipo Map.Entry possui alguns métodos como:
+
+            entry.getValue();       //* Retorna o valor do Map.Entry.
+            entry.getKey();         //* Retorna a chave do Map.Entry.
+            entry.setValue("haha"); //* Substitui o valor do Map.Entry.
         }
+//-----------------------------------------------------------------------------------------------------------------------------
+        //!COMO FUNCIONA KEY-SET + GET:
 
-        System.out.println("------------------------------------------------\n" +
-                           "Outra maneira de pegar chave e valor com for-each: ");
+        Set<String> chaves = map.keySet(); /// Retorna uma coleção Set (pois não pode haver chaves repetidas), com todas as chaves do map.
 
-        for (Map.Entry<String,String> entry: map.entrySet()){
-            System.out.printf("%s: %s%n",entry.getKey(),entry.getValue());
+        for (String chave : chaves) { /// Posso iterar sobre as chaves.
+
+            map.get(chave); /// Com o map.get, eu consigo retornar o valor de alguma chave.
         }
-
 
 
     }
--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/JMap02.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/JMap02.java
@@ -13,16 +13,18 @@ public class JMap02 {
 
 //Associando consumidores com seus respectivos produtos:
 
-
+        ///Consumidores:
         Consumidor consumidor1 = new Consumidor("Jusé");
         Consumidor consumidor2 = new Consumidor("Bernadito");
 
+        ///Produtos:
         Manga manga1 = new Manga(1l, "Yuru Camp", 25.99);
         Manga manga2 = new Manga(2l, "Bersek", 29.99);
         Manga manga3 = new Manga(3l, "Black Clover", 14.99);
         Manga manga4 = new Manga(4l, "Dragon Ball", 18.99);
         Manga manga5 = new Manga(5l, "Chainsaw Man", 9.99);
 
+        ///Associando consumidores à produtos:
         Map<Consumidor, Manga> consumidorMangaMap = new HashMap<>();
         consumidorMangaMap.put(consumidor1, manga1);
         consumidorMangaMap.put(consumidor2,manga5);
